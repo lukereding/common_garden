7d8352e15f59ee158d39cb253d82e9b02ac9c421
lukereding <lukereding@gmail.com>
2016-05-24 17:19:21 -0500

    various updates to script

--- locate_fish.py
+++ locate_fish.py
@@ -6,6 +6,7 @@ import json
 import os.path
 from Tkinter import *
 from tkFileDialog import askopenfilename
+import tkMessageBox
 
 '''
 
@@ -19,6 +20,114 @@ useage: python locate_fish.py video.mp4
 ##### py2exe && py2app
 
 '''
+
+# class to get the video from the user
+class get_video:
+    def __init__(self, master):
+        
+        def callback(self):
+            global video_name
+            video_name = self.askopenfilename()
+        global video_name
+        self.master = master
+        master.title("please choose the video file")
+
+        self.label = Label(master, text="please choose the video file to analyze")
+        self.label.pack()
+                
+        self.quit_button = Button(text='Close after file is chosen', command=master.quit).pack()
+        
+        video_name = askopenfilename()
+
+# class to create window to collect behavior observations from user
+class get_behaviors:
+    def __init__(self, master):
+        
+        self.master = master
+        master.title("behavior yourself!")
+
+        self.label = Label(master, text="enter in the number of behaviors in each box below")
+        self.label.pack()
+        
+        
+        # I can't find a better way around this stupidity (sorry):
+        self.label1 = Label( master, text="number of times a large male was aggressive towards another large male: ")
+        self.E1 = Entry(master, bd =5)
+        self.label1.pack()
+        self.E1.pack()
+        
+        self.label2 = Label( master, text="number of times a large male was aggressive towards a small male:  ")
+        self.E2 = Entry(master, bd =5)
+        self.label2.pack()
+        self.E2.pack()
+        
+        self.label12 = Label( master, text="number of times a large male was aggressive towards a female: ")
+        self.E12 = Entry(master, bd =5)
+        self.label12.pack()
+        self.E12.pack()
+        
+        self.label4 = Label( master, text="number of times a small male was aggressive towards a female:")
+        self.E4 = Entry(master, bd =5)
+        self.label4.pack()
+        self.E4.pack()
+        
+        self.label5 = Label( master, text="number of times an intermediate male was aggressive towards a female: ")
+        self.E5 = Entry(master, bd =5)
+        self.label5.pack()
+        self.E5.pack()
+        
+        self.label6 = Label( master, text="number of times an intermediate male was aggressive towards another intermediate male: ")
+        self.E6 = Entry(master, bd =5)
+        self.label6.pack()
+        self.E6.pack()
+        
+        self.label7 = Label( master, text="number of times a female was aggressive towards another female: ")
+        self.E7 = Entry(master, bd =5)
+        self.label7.pack()
+        self.E7.pack()
+        
+        self.label3 = Label( master, text="number of times a female was aggressive towards a male: ")
+        self.E3 = Entry(master, bd =5)
+        self.label3.pack()
+        self.E3.pack()
+        
+        self.label8 = Label( master, text="number of times a large male courted a female: ")
+        self.E8 = Entry(master, bd =5)
+        self.label8.pack()
+        self.E8.pack()
+        
+        self.label9 = Label( master, text="number of times an intermediate male courted a female:")
+        self.E9 = Entry(master, bd =5)
+        self.label9.pack()
+        self.E9.pack()
+        
+        self.label10 = Label( master, text="number of times a small male courted a female:")
+        self.E10 = Entry(master, bd =5)
+        self.label10.pack()
+        self.E10.pack()
+        
+        
+        self.save_button = Button(master, text="Submit responses", command=self.on_button)
+        self.save_button.pack()  
+        
+        self.quit_button = Button(master, text="Close", command=master.quit)
+        self.quit_button.pack()  
+                
+    def on_button(self):
+        global large_vs_large, large_vs_small, small_vs_female, int_vs_female, int_vs_int, female_vs_female, female_vs_male, large_court, int_court, small_court, large_vs_female
+        large_vs_large = self.E1.get()
+        large_vs_small = self.E2.get()
+        small_vs_female = self.E4.get()
+        int_vs_female = self.E5.get()
+        int_vs_int = self.E6.get()
+        female_vs_female = self.E7.get()
+        female_vs_male = self.E3.get()
+        large_court = self.E8.get()
+        int_court = self.E9.get()
+        small_court = self.E10.get()
+        large_vs_female = self.E12.get()
+
+
 
 # get the distance between the last element of the list and everything before it; repeat until the list is empty
 def pairwise_distance(locations):
@@ -73,19 +182,19 @@ def get_male_locations(frame):
 # mouse callback function
 def draw_circles_males(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(frame_copy,(x,y),5,(110, 85, 31),-1)
+        cv2.circle(frame_copy,(x,y),8,(110, 85, 31),-1)
         print x,y
         male_locations.append((x,y))
 
 def draw_circles_model_females(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(frame_copy,(x,y),5,(178,104,252),-1)
+        cv2.circle(frame_copy,(x,y),8,(178,104,252),-1)
         print "x : {}\ny: {}\n".format(x,y)
         model_female_locations.append((x,y))
 
 def draw_circles_focal_females(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(frame_copy,(x,y),5,(125,152,80),-1)
+        cv2.circle(frame_copy,(x,y),8,(125,152,80),-1)
         print "x : {}\ny: {}\n".format(x,y)
         focal_female_locations.append((x,y))
 
@@ -163,21 +272,15 @@ def get_info(text):
         var = raw_input('please only input a digit: ')
     return var
 
-def callback():
-    name = askopenfilename()
-    print name
-    return name
-
-def get_path():
-    Button(text='open file:', command=callback).pack(fill=X)
-    mainloop()
-
-print "here"
 
 if __name__ == "__main__":
 
     # read om the videoname
-    video_name = sys.argv[1]
+    root = Tk()
+    my_gui = get_video(root)
+    root.mainloop()
+    root.destroy()
+    print "video_name: {}".format(video_name)
 
     # show the video, on repeat if needed
     show_video(video_name)
@@ -203,36 +306,20 @@ if __name__ == "__main__":
     # close the windows`
     cv2.destroyAllWindows(); cv2.waitKey(1)
 
-    # get info on aggressive interactions:
-    print "aggressive interactions:\n\n",
-    large_vs_large = get_info("number of times a large male was aggressive towards another large male: ")
-    large_vs_small = get_info("number of times a large male was aggressive towards a small male: ")
-    large_vs_female = get_info("number of times a large male was aggressive towards a female: ")
-    small_vs_female = get_info("number of times a small male was aggressive towards a female: ")
-    int_vs_female = get_info("number of times an intermediate male was aggressive towards a female: ")
-    int_vs_int = get_info("number of times an intermediate male was aggressive towards another intermediate male: ")
-    female_vs_female = get_info("number of times a female was aggressive towards another female: ")
-    female_vs_male = get_info("number of times a female was aggressive towards a male : ")
-    large_courting = get_info("number of times a large male courted a female : ")
-    intermediate_courting = get_info("number of times an intermediate male courted a female : ")
-    small_courting = get_info("number of times a small male courted a female : ")
-
+    root = Tk()
+    my_gui = get_behaviors(root)
+    root.mainloop()
+    root.destroy()
+    
+    # get number of each type of fish
     number_focal = len(focal_female_locations)
     number_male = len(male_locations)
     number_model_female = len(model_female_locations)
-
-    tank_id = get_info("unique tank identifier (e.g. SS1) : ")
-
-    try:
-        from pick import pick
-        title = 'Please choose the treatment of the tank: '
-        options = ['all small males', 'all large males', 'large and small males', 'all intermediate males', 'all females', 'heterospecifics']
-        treatment, index = pick(options, title)
-        print "treatment : {}".format(treatment)
-    except:
-        pass
+    total_fish = number_focal + number_male + number_model_female
+    
     # get the data all together in a dictionary
     data = {
+    'video_name' : video_name,
     'focal_juvenile_locations' : focal_female_locations,
     'male_locations' : male_locations,
     'model_female_locations' : model_female_locations,
@@ -244,31 +331,37 @@ if __name__ == "__main__":
     'int_vs_female' : int_vs_female,
     'female_vs_female' : female_vs_female,
     'female_vs_male' : female_vs_male,
-    'large_courting' : large_courting,
-    'intermediate_courting' : intermediate_courting,
-    'small_courting' : small_courting,
+    'large_courting' : large_court,
+    'intermediate_courting' : int_court,
+    'small_courting' : small_court,
     'number_focal' : number_focal,
     'number_male' : number_male,
     'number_model_female' : number_model_female,
-    'tank_id' : tank_id,
-    'treatment' : treatment,
+    # 'tank_id' : tank_id,
+    # 'treatment' : treatment,
     'pairwise_distance_males' : pairwise_distance(male_locations),
     'pairwise_distance_females' : pairwise_distance(model_female_locations),
-    'pairewise_distance_juvs' : pairwise_distance(focal_female_locations)
+    'pairewise_distance_juvs' : pairwise_distance(focal_female_locations),
+    'total_fish' : total_fish
     }
-
+    
+    # figure out how to name the resulting file
     name = os.path.basename(video_name).split('.')[0] + '.json'
     i = 1
     while os.path.isfile(name):
         name = name.split('.')[0] + '_' + str(i) + '.json'
         i += 1
-
+    
+    # save the data file
     try:
         with open(name, 'w') as f:
             json.dump(data, f)
+        tkMessageBox.showinfo("oh yeahhh","\n\nall done. wahoo!\nthe data has been saved at {}".format(name))
     except:
         print "oh no! problem writing the data file. See Luke."
-
+        tkMessageBox.showinfo("oh no!","oh no! problem writing the data file. See Luke.")
+    
+    
     print "\n\nall done. wahoo!\nthe data has been saved at {}".format(name)
 
     cv2.destroyAllWindows()
