3c8630d0ec58a5c9a404289406f2a4a243bae4a5
Luke Reding <lukereding@dhcp-128-83-136-68.biosci.utexas.edu>
2016-06-02 11:33:49 -0500

    fix issue with pairwise dist when list empty

--- common_garden.py
+++ common_garden.py
@@ -24,7 +24,7 @@ useage: python locate_fish.py video.mp4
 # class to get the video from the user
 class get_video:
     def __init__(self, master):
-        
+
         def callback(self):
             global video_name
             video_name = self.askopenfilename()
@@ -34,85 +34,85 @@ class get_video:
 
         self.label = Label(master, text="please choose the video file to analyze")
         self.label.pack()
-                
+
         self.quit_button = Button(text='Close after file is chosen', command=master.quit).pack()
-        
+
         video_name = askopenfilename()
 
 # class to create window to collect behavior observations from user
 class get_behaviors:
     def __init__(self, master):
-        
+
         self.master = master
         master.title("behavior yourself!")
 
         self.label = Label(master, text="enter in the number of behaviors in each box below")
         self.label.pack()
-        
-        
+
+
         # I can't find a better way around this stupidity (sorry):
         self.label1 = Label( master, text="number of times a large male was aggressive towards another large male: ")
         self.E1 = Entry(master, bd =5)
         self.label1.pack()
         self.E1.pack()
-        
+
         self.label2 = Label( master, text="number of times a large male was aggressive towards a small male:  ")
         self.E2 = Entry(master, bd =5)
         self.label2.pack()
         self.E2.pack()
-        
+
         self.label12 = Label( master, text="number of times a large male was aggressive towards a female: ")
         self.E12 = Entry(master, bd =5)
         self.label12.pack()
         self.E12.pack()
-        
+
         self.label4 = Label( master, text="number of times a small male was aggressive towards a female:")
         self.E4 = Entry(master, bd =5)
         self.label4.pack()
         self.E4.pack()
-        
+
         self.label5 = Label( master, text="number of times an intermediate male was aggressive towards a female: ")
         self.E5 = Entry(master, bd =5)
         self.label5.pack()
         self.E5.pack()
-        
+
         self.label6 = Label( master, text="number of times an intermediate male was aggressive towards another intermediate male: ")
         self.E6 = Entry(master, bd =5)
         self.label6.pack()
         self.E6.pack()
-        
+
         self.label7 = Label( master, text="number of times a female was aggressive towards another female: ")
         self.E7 = Entry(master, bd =5)
         self.label7.pack()
         self.E7.pack()
-        
+
         self.label3 = Label( master, text="number of times a female was aggressive towards a male: ")
         self.E3 = Entry(master, bd =5)
         self.label3.pack()
         self.E3.pack()
-        
+
         self.label8 = Label( master, text="number of times a large male courted a female: ")
         self.E8 = Entry(master, bd =5)
         self.label8.pack()
         self.E8.pack()
-        
+
         self.label9 = Label( master, text="number of times an intermediate male courted a female:")
         self.E9 = Entry(master, bd =5)
         self.label9.pack()
         self.E9.pack()
-        
+
         self.label10 = Label( master, text="number of times a small male courted a female:")
         self.E10 = Entry(master, bd =5)
         self.label10.pack()
         self.E10.pack()
-        
-        
+
+
         self.save_button = Button(master, text="Submit responses", command=self.on_button)
-        self.save_button.pack()  
-        
+        self.save_button.pack()
+
         self.quit_button = Button(master, text="Close", command=master.quit)
-        self.quit_button.pack()  
-                
+        self.quit_button.pack()
+
     def on_button(self):
         global large_vs_large, large_vs_small, small_vs_female, int_vs_female, int_vs_int, female_vs_female, female_vs_male, large_court, int_court, small_court, large_vs_female
         large_vs_large = self.E1.get()
@@ -261,7 +261,7 @@ def show_video(name, size):
                 if size == "small":
                     frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
                 cv2.putText(frame,'press the escape key when done',(20,20), cv2.FONT_HERSHEY_SIMPLEX, 1,(130,130,130),2)
-                
+
                 cv2.imshow(name,frame)
             except:
                 cap = cv2.VideoCapture(name)
@@ -287,7 +287,7 @@ def check_if_data_dir_exists(p):
     if not os.path.exists(os.path.join(p,'data')):
         os.makedirs(os.path.join(p,'data'))
     return os.path.join(p,'data')
-    
+
 def check_if_already_watched_exists(p):
     if not os.path.exists(os.path.join(p,'already_watched')):
         os.makedirs(os.path.join(p,'already_watched'))
@@ -306,7 +306,7 @@ def get_screen_dim(r):
     return screen_width, screen_height, size
 
 if __name__ == "__main__":
-    
+
     # read om the videoname
     root = Tk()
     # get screen dimensions
@@ -344,37 +344,40 @@ if __name__ == "__main__":
     my_gui = get_behaviors(root2)
     root2.mainloop()
     root2.destroy()
-    
+
     # get number of each type of fish
     number_focal = len(focal_female_locations)
     number_male = len(male_locations)
     number_model_female = len(model_female_locations)
     total_fish = number_focal + number_male + number_model_female
-    
+
+    # if veiwing on a small screen, double the values of the points to keep things consistent
     if size == "small":
         focal_female_locations = double_list(focal_female_locations)
         male_locations = double_list(male_locations)
         model_female_locations = double_list(model_female_locations)
-    
+
     # the videos are named like 'week_of_04_03_2016_SS1_00:01'. let's extract the date and tank id from that
     path, video = os.path.split(video_name)
-    
+
     # make sure it comforms to our naming convention:
     if len(video.split("_")) == 7:
         # get date
         date = video.split('_')[2:5]
         date = '-'.join(date)
-        
+
         # get tank id
         tank_id = video.split('_')[-2].upper()
-        
+
         # get when in the video
         time_in_video = video.split('_')[-1].split('.')[0]
-    
+
     # otherwise, call assign None
     else:
-        date = tank_id = time_in_video = None    
-    
+        date = tank_id = time_in_video = None
+        ## get date from file!!
+        #############
+
     # get the data all together in a dictionary
     data = {
     'video_name' : video_name,
@@ -396,16 +399,16 @@ if __name__ == "__main__":
     'number_male' : number_male,
     'number_model_female' : number_model_female,
     'tank_id' : tank_id,
-    'pairwise_distance_males' : pairwise_distance(male_locations),
-    'pairwise_distance_females' : pairwise_distance(model_female_locations),
-    'pairewise_distance_juvs' : pairwise_distance(focal_female_locations),
+    'pairwise_distance_males' : pairwise_distance(male_locations) if male_locations is not None else None,
+    'pairwise_distance_females' : pairwise_distance(model_female_locations) if model_female_locations is not None else None,
+    'pairewise_distance_juvs' : pairwise_distance(focal_female_locations) if focal_female_locations is not None else None,
     'total_fish' : total_fish,
     'time_of_clip' : time_in_video,
     'date' : date,
     'size_of_screen': size,
     'info': 'distances were multiplied by two if the user was on a small screen'
     }
-    
+
     # figure out how to name the resulting file
     name = video.split('.')[0] + '.json'
     data_dir = check_if_data_dir_exists(path)
@@ -413,24 +416,24 @@ if __name__ == "__main__":
     while os.path.isfile(data_dir + '/' + name):
         name = video.split('.')[0] + '_' + str(i) + '.json'
         i += 1
-    
+
     # save the data file in the data directory, first making sure it exists
     print data_dir + '/' + name
     try:
         with open(data_dir + '/' + name, 'w') as f:
             json.dump(data, f)
-        
+
         # then move the file to the 'already_watched' directory
         save_to = check_if_already_watched_exists(path)
         print save_to
-        
+
         os.rename(video_name, os.path.join(save_to, video))
         tkMessageBox.showinfo("oh yeahhh","\n\nall done. wahoo!\nthe data has been saved at {}".format(data_dir + '/' + name))
     except:
         print "oh no! problem writing the data file. See Luke."
         tkMessageBox.showinfo("oh no!","oh no! problem writing the data file. See Luke.")
-    
-    
+
+
     print "\n\nall done. wahoo!\nthe data has been saved at {}".format(data_dir + '/' + name)
 
     cv2.destroyAllWindows()
